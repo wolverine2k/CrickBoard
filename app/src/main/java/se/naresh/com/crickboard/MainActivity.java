9d6ee6502e82cbfbbb55f123a953cab43d7c5ce8
Naresh Mehta <nareshtechs@gmail.com>
2015-06-09 12:47:30 +0200

    Introduction of the Navigation Drawer Part1

--- app/src/main/java/se/naresh/com/crickboard/MainActivity.java
+++ app/src/main/java/se/naresh/com/crickboard/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CrickDBHelperOrm db = new CrickDBHelperOrm(this);
         Log.d(LOG_TAG, "MainActivity Started...");
     }
 
